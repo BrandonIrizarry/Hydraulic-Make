0d69d0b7e4754c086ff65665b54aa07c0e831e4d
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-10-28 19:43:13 -0400

    tests: Update with new API

--- tests.el
+++ tests.el
@@ -33,18 +33,49 @@
     (should (equal (get-file penv example-file :type 'basename)
                    "CircleOptions"))))
 
-(ert-deftest mapapp-deps ()
-  "A mock main function to test the current state of the
-application."
-  :tags '(main)
-  (add-to-list 'load-path (expand-file-name ".") t)
-  (require 'package-table)
+(let ((penv (project-environment-create "~/eclipse-workspace2/UCSDGraphs/" "src/" "bin/")))
+  (ert-deftest mapapp-deps ()
+    "Examine the dependencies of 'application/MapApp.java' (UCSD
+project)."
+    :tags '(duke)
+    (should (equal (list-deps penv "application/MapApp.java")
+                   '("MarkerManager" "SelectManager" "geography.GeographicPoint" "CLabel" "DataSet" "gmapsfx.javascript.object.MapTypeIdEnum" "gmapsfx.javascript.object.MapOptions" "gmapsfx.javascript.object.LatLong" "gmapsfx.javascript.object.GoogleMap" "gmapsfx.MapComponentInitializedListener" "gmapsfx.GoogleMapView" "application.services.RouteService" "application.services.GeneralService" "application.controllers.RouteController" "application.controllers.FetchController")))))
 
-  (let* ((penv (project-environment-create "~/eclipse-workspace2/UCSDGraphs/" "src/" "bin/"))
-         (ptable (package-table-create penv)))
-    (should (equal (map-pairs (find-inline-dependencies ptable "application/MapApp.java"))
-                   '(("application.controllers" . t)
-                     ("application.services" . t)
-                     ("gmapsfx" . t)
-                     ("gmapsfx.javascript.object" . t)
-                     ("geography" . t))))))
+;;; WEATHER DATA (DUKE)
+(let ((penv (project-environment-create "~/Java/DukeIntro/Course_2/parsing_weather_data/" "" "bin/")))
+  (ert-deftest weather-default-package-p ()
+    "'Weather.java' should be in the default package, which should be
+the empty string."
+    :tags '(duke)
+    (should (equal "default"
+                   (get-package penv "Weather.java"))))
+
+  (ert-deftest weather-deps ()
+    "Examine the dependencies of '~/Java/DukeIntro/Course_2/parsing_weather_data/Weather.java'."
+    :tags '(duke)
+    (should (equal (list-deps penv "Weather.java")
+                   nil)))
+
+  (ert-deftest check-package-table-weather ()
+    "Check the (rather simple) package table used by this project."
+    :tags '(duke)
+    (let ((ptable (package-table-create penv)))
+      (should (equal (map-pairs (package-table-table ptable))
+                     '(("default" "Weather.java")))))))
+
+;; WORDNGRAM (DUKE)
+
+(let ((penv (project-environment-create "~/Java/DukeIntro/Course_4/week_3/WordNGramStarterProgram/" "" "bin/")))
+  (ert-deftest check-package-table-ngram-main ()
+    "Check the package table used by this project."
+    :tags '(duke)
+    (let ((ptable (package-table-create penv)))
+      (should (equal (mapcar (lambda (pair) (sort pair #'string<))
+                             (map-pairs (package-table-table ptable)))
+                     '(("IMarkovModel.java" "Main.java" "MarkovWordOne.java" "MarkovWordTwo.java" "default"))))))
+
+  (ert-deftest wordngram-deps ()
+    "Examine the dependencies of '~/Java/DukeIntro/Course_4/week_3/WordNGramStarterProgram/Main.java'."
+    :tags '(duke)
+    (should (equal (list-deps penv "Main.java")
+                   '("MarkovWordTwo" "IMarkovModel")))))
