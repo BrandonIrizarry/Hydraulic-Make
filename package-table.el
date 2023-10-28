9ecde28cd571bd820117ae61149c40951e76d836
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-10-28 16:36:17 -0400

    Revise definition of 'package-table'

--- package-table.el
+++ package-table.el
@@ -4,16 +4,22 @@
 
 (cl-defstruct (package-table (:constructor package-table--create))
   "A hash table mapping a package prefix to the files it encompasses."
-  table)
+  penv table)
 
 (cl-defmethod package-table-create ((this project-environment))
   "The public constructor for PACKAGE-TABLE objects."
-  (let ((known-packages (make-hash-table :test #'equal)))
-    (dolist (file (project-environment-files this) known-packages)
-      (let* ((package (get-package this file))
-             (files (gv-ref (gethash package known-packages (list)))))
-        (puthash package (cons file (gv-deref files)) known-packages)))
-    (package-table--create :table known-packages)))
+  (let ((ptable (package-table--create
+                 :penv this)))
+    (let ((known-packages (make-hash-table :test #'equal)))
+      (dolist (file (project-environment-files this) known-packages)
+        (let* ((package (get-package this file))
+               (files (gv-ref (gethash package known-packages (list)))))
+          (puthash package (cons file (gv-deref files)) known-packages)))
+      (setf (package-table-table ptable) known-packages)
+      ptable)))
+
+(cl-defmethod get-file ((this package-table) package-path &key type)
+  (get-file (package-table-penv this) package-path :type type))
 
 (cl-defmethod pretty-print ((this package-table))
   "Pretty-print PACKAGE-TABLE."
