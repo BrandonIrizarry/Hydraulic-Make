b68a51a986d58093d2a940ea853007fc68081a21
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-10-28 12:27:22 -0400

    Refactor 'create' function to use 'project-environment'

--- package-table.el
+++ package-table.el
@@ -4,27 +4,22 @@
 
 (cl-defstruct (package-table (:constructor package-table--create))
   "A hash table mapping a package prefix to the files it encompasses."
-  hash-table assoc-table)
+  table)
 
-(defun package-table-create (project-root package-subdir class-subdir)
+(cl-defmethod package-table-create ((this project-environment))
   "The public constructor for PACKAGE-TABLE objects."
-  (let ((source-files (java-project-get-source-files project-root package-subdir class-subdir))
-        (known-packages (make-hash-table :test #'equal)))
-    (dolist (source-file source-files known-packages)
-      (let* ((package-prefix (get-package-prefix source-file))
-             ;; Get the reference to the list of files under this
-             ;; prefix, to make adding to it less verbose.
-             (files (gv-ref (gethash package-prefix known-packages (list)))))
-        ;; Add the current project file under the corresponding
-        ;; package prefix.
-        (puthash package-prefix (cons source-file (gv-deref files)) known-packages)))
-    (package-table--create :hash-table known-packages :assoc-table (map-pairs known-packages))))
+  (let ((known-packages (make-hash-table :test #'equal)))
+    (dolist (file (project-environment-files this) known-packages)
+      (let* ((package (get-package this file))
+             (files (gv-ref (gethash package known-packages (list)))))
+        (puthash package (cons file (gv-deref files)) known-packages)))
+    (package-table--create :table known-packages)))
 
 (cl-defmethod pretty-print ((this package-table))
   "Pretty-print PACKAGE-TABLE."
   (mapcar (pcase-lambda (`(,package-name . ,package-files))
-              (cons package-name (mapcar #'pretty-print package-files)))
-          (package-table-assoc-table this)))
+              (cons package-name package-files))
+          (map-pairs (package-table-table this))))
 
 (cl-defmethod get-files ((this package-table) package-prefix)
   "Given PACKAGE-PREFIX, get the list of files (as file-selector
