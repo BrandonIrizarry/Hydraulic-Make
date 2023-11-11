11e9de0c63e50ba5ed16b52f7ea68e6b95710790
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-11-11 18:04:28 -0500

    Rename Eshell commands according to README

--- package-table.el
+++ package-table.el
@@ -172,7 +172,7 @@ environment."
       (let ((command (list (format "javac -g -cp \"lib/*:%s\" -d %s" class-subdir class-subdir))))
         (mapconcat #'identity (append command full-filenames) " ")))))
 
-(defun eshell/setup-java-invocation (&rest args)
+(defun eshell/hmake-setup (&rest args)
   ;; I'm not sure if it's possible to temporarily set
   ;; ELISP-LISP-REGEXP for the scope of these calls, so we must intern
   ;; keyword-like strings manually.
@@ -192,19 +192,19 @@ environment."
            (package-subdir (or (plist-get kwargs :src) "src/"))
            (class-subdir (or (plist-get kwargs :bin) "bin/")))
       (eshell-print (format "project root: '%s'\nsource directory: '%s'\nclass-file directory: '%s'\n----\n" project-root package-subdir class-subdir))
-      (--setup-java-invocation project-root package-subdir class-subdir))))
+      (hmake--setup project-root package-subdir class-subdir))))
 
-(defun --setup-java-invocation (project-root package-subdir class-subdir)
+(defun hmake--setup (project-root package-subdir class-subdir)
   "Use an approach where the build and run commands are predefined
 by this setup command."
   (let ((penv (project-environment-create (expand-file-name project-root) package-subdir class-subdir)))
 
-    (defun eshell/java-build (target)
+    (defun eshell/hmake-build-command (target)
       "Insert the appropriate build command into the Eshell prompt."
 
       ;; Let's define this right away, so that we can use it to insert
       ;; the run command right away if there's nothing to build.
-      (defun eshell/java-run ()
+      (defun eshell/hmake-run-command ()
         "Insert the appropriate run command into the Eshell prompt."
         (eshell-kill-input)
         (let ((package (get-package penv target)))
@@ -219,6 +219,6 @@ by this setup command."
         (if invocation
             (insert invocation)
           (eshell-print "Nothing to build; inserting run command\n----\n")
-          (eshell/java-run))))))
+          (eshell/hmake-run-command))))))
 
 (provide 'package-table)
