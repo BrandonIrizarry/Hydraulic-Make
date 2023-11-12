b337ac4a3cb98aacda49f94a5ff2ee0a7ae9fc86
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-11-11 21:31:38 -0500

    Update 'hmake-tests.el' with our current naming conventions
    That is, add the "hmake-" prefix to everything.
    
    I've also deleted some unnecessary stuff at the beginning of the file.

--- hmake-tests.el
+++ hmake-tests.el
@@ -1,11 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 
-;;; TESTS FOR 'FILENAME-SELECTOR.EL'
-
-(add-to-list 'load-path (expand-file-name ".") t)
-(require 'package-table)
+(require 'h-package-table)
 (require 'ert-fixtures)
-
 
 (defvar ht-ucsd-defs
   (efs-define-fixture
@@ -15,26 +11,26 @@
        (example-file "gmapsfx/shapes/CircleOptions.java")
        (example-class "gmapsfx/shapes/CircleOptions.class")
        (example-package-unit "gmapsfx.shapes.CircleOptions")
-       (penv (project-environment-create project-root "src/" "bin/")))))
+       (penv (h-project-environment-create project-root "src/" "bin/")))))
 
 (efs-use-fixtures get-full-filename (ht-ucsd-defs)
-  (should (equal (get-file penv example-file :type 'full)
+  (should (equal (h-get-file penv example-file :type 'full)
                  (concat package-root example-file))))
 
 (efs-use-fixtures get-path-to-class-file (ht-ucsd-defs)
-  (should (equal (get-file penv example-file :type 'class)
+  (should (equal (h-get-file penv example-file :type 'class)
                  (concat class-root example-class))))
 
 (efs-use-fixtures get-package-unit (ht-ucsd-defs)
-  (should (equal (get-file penv example-file :type 'package)
+  (should (equal (h-get-file penv example-file :type 'package)
                  "gmapsfx.shapes.CircleOptions")))
 
 (efs-use-fixtures get-package (ht-ucsd-defs)
-  (should (equal (get-package penv example-file)
+  (should (equal (h-get-package penv example-file)
                  "gmapsfx.shapes")))
 
 (efs-use-fixtures get-basename (ht-ucsd-defs)
-  (should (equal (get-file penv example-file :type 'basename)
+  (should (equal (h-get-file penv example-file :type 'basename)
                  "CircleOptions")))
 
 
@@ -42,14 +38,14 @@
   "Examine the dependencies of 'application/MapApp.java' (UCSD
 project)."
   :tags '(duke)
-  (should (equal (list-deps penv "application/MapApp.java")
+  (should (equal (h-list-deps penv "application/MapApp.java")
                  '("application.MarkerManager" "application.SelectManager" "geography.GeographicPoint" "application.CLabel" "application.DataSet" "gmapsfx.javascript.object.MapTypeIdEnum" "gmapsfx.javascript.object.MapOptions" "gmapsfx.javascript.object.LatLong" "gmapsfx.javascript.object.GoogleMap" "gmapsfx.MapComponentInitializedListener" "gmapsfx.GoogleMapView" "application.services.RouteService" "application.services.GeneralService" "application.controllers.RouteController" "application.controllers.FetchController"))))
 
 ;;; WEATHER DATA (DUKE)
 
 (defvar ht-duke-weather-defs
   (efs-define-fixture
-      ((penv (project-environment-create "~/Java/DukeIntro/Course_2/parsing_weather_data/" "" "bin/")))))
+      ((penv (h-project-environment-create "~/Java/DukeIntro/Course_2/parsing_weather_data/" "" "bin/")))))
 
 
 (efs-use-fixtures weather-default-package-p (ht-duke-weather-defs)
@@ -57,65 +53,64 @@ project)."
 the empty string."
   :tags '(duke)
   (should (equal "default"
-                 (get-package penv "Weather.java"))))
+                 (h-get-package penv "Weather.java"))))
 
 (efs-use-fixtures weather-deps (ht-duke-weather-defs)
   "Examine the dependencies of '~/Java/DukeIntro/Course_2/parsing_weather_data/Weather.java'."
   :tags '(duke)
-  (should (equal (list-deps penv "Weather.java")
+  (should (equal (h-list-deps penv "Weather.java")
                  nil)))
 
 (efs-use-fixtures check-package-table-weather (ht-duke-weather-defs)
   "Check the (rather simple) package table used by this project."
   :tags '(duke)
-  (let ((ptable (package-table-create penv)))
-    (should (equal (map-pairs (package-table-table ptable))
+  (let ((ptable (h-package-table-create penv)))
+    (should (equal (map-pairs (h-package-table-table ptable))
                    '(("default" "Weather.java"))))))
 
 ;; WORDNGRAM (DUKE)
 
 (defvar ht-duke-wordgram-defs
   (efs-define-fixture
-      ((penv (project-environment-create "~/Java/DukeIntro/Course_4/week_3/WordNGramStarterProgram/" "" "bin/")))))
+      ((penv (h-project-environment-create "~/Java/DukeIntro/Course_4/week_3/WordNGramStarterProgram/" "" "bin/")))))
 
 
 (efs-use-fixtures check-package-table-ngram-main (ht-duke-wordgram-defs)
   "Check the package table used by this project."
   :tags '(duke)
-  (let ((ptable (package-table-create penv)))
+  (let ((ptable (h-package-table-create penv)))
     (should (equal (mapcar (lambda (pair) (sort pair #'string<))
-                           (map-pairs (package-table-table ptable)))
+                           (map-pairs (h-package-table-table ptable)))
                    '(("IMarkovModel.java" "Main.java" "MarkovWordOne.java" "MarkovWordTwo.java" "default"))))))
 
 (efs-use-fixtures wordngram-deps (ht-duke-wordgram-defs)
   "Examine the dependencies of '~/Java/DukeIntro/Course_4/week_3/WordNGramStarterProgram/Main.java'."
   :tags '(duke deps)
-  (should (equal (list-deps penv "Main.java")
+  (should (equal (h-list-deps penv "Main.java")
                  '("default.MarkovWordTwo" "default.IMarkovModel"))))
 
 (defvar ht-fakeproject-defs
   (efs-define-fixture
-      ((penv (project-environment-create "~/Java/FakeProject/" "src/" "bin/")))))
+      ((penv (h-project-environment-create "~/Java/FakeProject/" "src/" "bin/")))))
 
 (efs-use-fixtures conference-deps (ht-fakeproject-defs)
   "Examine the dependencies of 'conference/Conference.java'."
   :tags '(me deps)
-  (should (equal (list-deps penv "conference/Conference.java")
+  (should (equal (h-list-deps penv "conference/Conference.java")
                  '("asturias.Cangas" "asturias.Gijon" "andaluz.Malaga" "andaluz.Sevilla"))))
 
 ;; Dependency graph.
-(defvar ht-ucsd-dep-graph (efs-define-fixture ((graph (dependency-graph-create penv)))))
+(defvar ht-ucsd-dep-graph (efs-define-fixture ((graph (h-dependency-graph-create penv)))))
 
 (efs-use-fixtures create-dep-graph (ht-ucsd-defs ht-ucsd-dep-graph)
   :tags '(ucsd edebug)
-  (get-dependencies graph "application/MapApp.java")
-  (get-dependencies graph "application/RouteVisualization.java"))
+  (h-get-dependencies graph "application/MapApp.java")
+  (h-get-dependencies graph "application/RouteVisualization.java"))
 
 (efs-use-fixtures check-modified-deps (ht-ucsd-defs)
   :tags '(ucsd edebug)
-  (get-modified-dependencies penv "application/MapApp.java"))
-
+  (h-get-modified-dependencies penv "application/MapApp.java"))
 
 ;; Local Variables:
-;; read-symbol-shorthands: (("efs-" . "ert-fixtures-") ("ht-" . "hmake-tests-"))
+;; read-symbol-shorthands: (("efs-" . "ert-fixtures-") ("h-" . "hmake-") ("ht-" . "hmake-tests-"))
 ;; End:
