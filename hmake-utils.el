d8285a64ec941c58cc5906e839a091f7c57b43dc
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-11-13 21:08:05 -0500

    Remove "temp-buffer" restriction on preprocessing-utility

--- hmake-utils.el
+++ hmake-utils.el
@@ -1,26 +1,24 @@
 ;; -*- lexical-binding: t; -*-
 
 (defun hu-strip-non-code-artefacts ()
-  "Strip the current temporary buffer of non-code artefacts."
-  (when (string-match-p (rx bos " *temp*") (buffer-name))
-    (save-excursion
-      (rx-let ((java-multi-line-comment
-                (: "/*" (*
-                         (| (not "*")
-                            (: "*" (not "/"))))
-                   (+ "*") "/"))
-               (java-line-comment
-                (: "//" (* not-newline) eol))
-               (java-string
-                (: ?\" (*? anything) ?\")))
-        (while (re-search-forward (rx (or java-multi-line-comment
-                                          java-line-comment
-                                          java-string))
-                                  nil t)
-          (replace-match ""))))))
-
 (defun hu-get-current-line ()
   "Return the line POINT is on in the current temporary buffer."
+  "Strip the current buffer of non-code artefacts."
+  (save-excursion
+    (rx-let ((java-multi-line-comment
+              (: "/*" (*
+                       (| (not "*")
+                          (: "*" (not "/"))))
+                 (+ "*") "/"))
+             (java-line-comment
+              (: "//" (* not-newline) eol))
+             (java-string
+              (: ?\" (*? anything) ?\")))
+      (while (re-search-forward (rx (or java-multi-line-comment
+                                        java-line-comment
+                                        java-string))
+                                nil t)
+        (replace-match "")))))
   (string-trim (buffer-substring-no-properties (line-beginning-position)
                                                (line-end-position))))
 
